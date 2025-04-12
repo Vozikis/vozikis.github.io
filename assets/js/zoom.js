4f0ce16614d11275fb93e3ea920d55965ef95d82
Vozikis <Vozikisant@gmail.com>
2025-04-12 03:08:56 +0200

    zoomable remove

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,6 +1,15 @@
 // Initialize medium zoom.
+// $(document).ready(function () {
+//   medium_zoom = mediumZoom("[data-zoomable]", {
+//     background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
+//   });
+// });
+
+
+// Initialize medium zoom.
 $(document).ready(function () {
-  medium_zoom = mediumZoom("[data-zoomable]", {
+  medium_zoom = mediumZoom("[data-zoomable]:not(.preview)", {
     background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
   });
 });
+
